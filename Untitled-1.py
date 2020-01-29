6d01f3655501eaf3c5be06693c7b0a78a57a8f42
tatlisumak <45922033+tatlisumak@users.noreply.github.com>
2020-01-29 11:21:26 +0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,20 @@
-name = "Abdullah"
-print(name)
+"""liste = [1,1]
+for i in range(2,10):
+    liste.append(liste[i-1]+liste[i-2])
+print(liste)
+
+list1=[1,1]
+while len(list1) < 10:
+    list1.append(list1[-1]+list1[-2])
+
+
+
+print(list1)"""
+
+def fib(n):
+    list1=[1,1]
+    while len(list1) < n:
+        list1.append(list1[-1]+list1[-2])
+    return list1
+
+print(fib(5))
